0f82e3d5b8b1ebf5c1c5720f79497b96d48bca46
rdraghul <rdraghul@icloud.com>
2017-10-03 14:37:13 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,10 @@
+
+public class bitwise {
+	public static void main(String[] s){
+		int a=5,b=6;
+		 System.out.println( "a AND b Bitwise : " + 
+		         Integer.toBinaryString( a & b ));
+		 int c = a&b;
+		 System.out.println( "a AND b Logical : " + c);
+	}
+}
